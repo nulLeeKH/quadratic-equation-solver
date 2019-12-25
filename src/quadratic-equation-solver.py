9e9db763d431f258040376ae6f873d097e4814d6
nulLeeKH <i_am@nulleekh.com>
2019-12-26 02:29:16 +0900

    [FEATURE] Add calculation and display result window feature

--- src/quadratic-equation-solver.py
+++ src/quadratic-equation-solver.py
@@ -25,10 +25,12 @@ SOFTWARE.
 """
 
 import sys
+from math import pow, sqrt
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
 
 main_window_ui = uic.loadUiType("gui/main_window.ui")[0]
+result_window_ui = uic.loadUiType("gui/result_window.ui")[0]
 
 
 class MainWindow(QMainWindow, main_window_ui):
@@ -36,6 +38,54 @@ class MainWindow(QMainWindow, main_window_ui):
         super().__init__()
         self.setupUi(self)
         self.move(QDesktopWidget().availableGeometry().center() - self.frameGeometry().center())
+
+        self.calculateButton.clicked.connect(self.calculate_clicked)
+
+    def calculate_clicked(self):
+        second_coefficient = float(self.secondCoefficient.text())
+        first_coefficient = float(self.firstCoefficient.text())
+        constant = float(self.constant.text())
+
+        if second_coefficient == 0:
+            if first_coefficient == 0:
+                result_0 = None
+                result_1 = result_0
+            else:
+                result_0 = -constant / first_coefficient
+                result_1 = result_0
+        else:
+            result_0 = (-first_coefficient + (sqrt(pow(first_coefficient, 2) - (second_coefficient * constant)))) / second_coefficient
+            result_1 = (-first_coefficient - (sqrt(pow(first_coefficient, 2) - (second_coefficient * constant)))) / second_coefficient
+
+        if result_0 is None and result_0 == result_1:
+            result = "ERROR: No x in equation."
+        elif result_0 == result_1:
+            result = str(round(result_0, 3)) + ' (multiple root)'
+        else:
+            result = str(round(result_0, 3)) + ' or ' + str(round(result_1, 3))
+
+        result_window = Result()
+        result_window.represent_result(result)
+        result_window.exec_()
+
+
+class Result(QDialog, result_window_ui):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        # Setup ui.
+        self.move(QDesktopWidget().availableGeometry().center() - self.frameGeometry().center())
+        # Move window to center.
+        self.doneButton.clicked.connect(self.done_clicked)
+        # Call done_clicked function when done button clicked.
+
+    def done_clicked(self):
+        self.close()
+        # Close window.
+
+    def represent_result(self, result):
+        self.result.setText(result)
+        # Print out result data.
 
 
 if __name__ == "__main__":
